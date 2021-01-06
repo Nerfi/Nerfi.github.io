a88cfa199667a147b12566dd46841db927830601
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-01-06 11:32:20 +0100

    added GetForm HTML

--- src/index.js
+++ src/index.js
@@ -54,4 +54,28 @@ const sr = ScrollReveal({
     reset: true //esta propiedad va a hacer que esto se repita en bucle
 })
 
-sr.reveal('.home__title')
+sr.reveal('.home__title');
+sr.reveal('.home__scroll', {delay: 250});
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+
+// About section scroll
+sr.reveal( '.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
